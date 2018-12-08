19d0374dc9dff9083edbc8a6b5b7a27a348c3e64
ema kolaric <emakolaric@icloud.com>
2018-12-08 01:03:37 +0100

    Razredi, IComparable

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -10,6 +10,27 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+            Razlomak r1 = new Razlomak(5, 20);
+            Razlomak r2 = new Razlomak(1, 9);
+            Razlomak r3 = new Razlomak(22, 20);
+            List<Razlomak> raz = new List<Razlomak>();
+            raz.Add(r1);
+            raz.Add(r2);
+            raz.Add(r3);
+            foreach(Razlomak r in raz)
+            {
+                Console.WriteLine("Razlomak : {0}/{1}, njegova decimalna vrijednost: {2}" , r.Brojnik, r.Nazivnik,r.PretvoriUFloat());  
+            }
+
+            raz.Sort();
+
+            Console.WriteLine("Poslije sortiranja: ");
+
+            foreach (Razlomak r in raz)
+            {
+                Console.WriteLine("Razlomak : {0}/{1}, njegova decimalna vrijednost: {2}", r.Brojnik, r.Nazivnik, r.PretvoriUFloat());
+            }
+            
         }
     }
 }
---
+++ ConsoleApp4/Razlomak.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Razlomak :IComparable<Razlomak>
+    {
+        private int brojnik;
+        private int nazivnik;
+
+        public Razlomak (int _brojnik, int _nazivnik)
+        {
+            brojnik = _brojnik;
+            nazivnik = _nazivnik;
+        }
+        public int Brojnik 
+        {
+            set => brojnik = value;
+            get => brojnik;
+        }
+        public int Nazivnik
+        {
+            set => nazivnik = value;
+            get => nazivnik;
+        }
+        public float PretvoriUFloat()
+        {
+            return (float)brojnik / nazivnik;
+        }
+
+        public bool Ispravan()
+        {
+            if (nazivnik==0)
+            
+                return false;
+            
+            else
+                return true;
+        }
+
+        protected int nzd()
+        {
+            int br = 0;
+            if (nazivnik > brojnik)
+            {
+                for (int x = nazivnik; x > 0; x--)
+                {
+                    if (brojnik % x == 0 && nazivnik % x == 0)
+                    {
+                        br = x;
+                        break;
+                    }
+
+                }
+            }
+            if (brojnik > nazivnik)
+            {
+                for (int x = brojnik; x > 0; x--)
+                {
+                    if (brojnik % x == 0 && nazivnik % x == 0)
+                    {
+                        br = x;
+                        break;
+                    }
+
+                }
+            }
+            return br;
+        }
+
+        public string SkratiRazlomak()
+        {
+            string z = brojnik / nzd() + "/" + nazivnik / nzd();
+            return z;
+        }
+
+        public int CompareTo(Razlomak other)
+        {
+            return this.PretvoriUFloat().CompareTo(other.PretvoriUFloat());
+        }
+
+    }
+}
